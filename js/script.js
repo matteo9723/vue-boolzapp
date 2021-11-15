be07073562d10f304d547d8557c28ce5105f89f1
matteo9723 <matteolandri7@gmail.com>
2021-11-15 11:18:13 +0100

    firstCommit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const app = new Vue ({
+  
+  el: '#app',
+
+  data: {
+    saluto:'ciao'
+   },
+
+  mounted(){},
+
+  methods:{}
+
+});
+
+  
+
+
+
+// SHORTCUT --> ctrl+F = sostituisci i caratteri uguali velocemente
+  
